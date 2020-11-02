01cce29ffe66c9b6b014b00b83ef929f6a9eb2ef
cocoMango05 <69894363+cocoMango05@users.noreply.github.com>
2020-11-02 16:05:39 +0200

    Add files via upload
    Multiball powerup, Paddle growth, Key powerup obtained, Key block unlocked.

---
+++ src/Powerup.lua
@@ -0,0 +1,55 @@
+ --spawns powerup 
+ -- if ball has hit block n times then as soon as ball collides with paddle 2 more balls spawn and behave like first
+ --Balls reset when player entes victory or loss/start over state 
+
+Powerup = Class{}
+
+function Powerup:init(powertype, x, y)
+     -- simple positional and dimensional variables
+     self.width = 16
+     self.height = 16
+ 
+     -- these variables are for keeping track of our velocity on both the
+     -- X and Y axis, since the ball can move in two dimensions
+     self.dy = 40
+     self.dx = 0
+     self.x = x
+     self.y = y
+
+     -- this is the type of powerup, and we will index
+     -- our table of Quads relating to the global block texture using this
+     self.powertype = powertype
+     self.inplay = true
+
+end
+
+function Powerup:collides(target)
+    if self.x > target.x + target.width or target.x > self.x + self.width then
+        return false
+    end
+
+    -- then check to see if the bottom edge of either is higher than the top
+    -- edge of the other
+    if self.y > target.y + target.height or target.y > self.y + self.height then
+        return false
+    end 
+
+    -- if the above aren't true, they're overlapping
+    return true
+end
+
+
+function Powerup:update(dt)
+
+    if self.y < VIRTUAL_HEIGHT then
+        self.y = self.y + self.dy * dt
+    end
+
+end
+
+
+function Powerup:render()
+    if self.inplay then
+        love.graphics.draw(gTextures['main'], gFrames['power'][self.powertype], self.x, self.y)
+    end
+end
